d672088300928bc314a15a03ba4eb461f0079875
gozineb <zinebe@theodo.fr>
2023-10-05 11:27:00 +0200

    🧑‍💻 add black formatter

--- demo.py
+++ demo.py
@@ -8,13 +8,14 @@ st.set_page_config(page_title="Audio Recorder and Chat")
 # Create a state variable to control page navigation
 page = st.sidebar.radio("Select a page", ["Audio Recorder", "Chat Page"])
 
+
 def audiorec_demo_app():
     """
     Render the Audio Recorder page.
     """
     # TITLE and Creator information
-    st.title('Audio Recorder')
-    st.write('\n\n')
+    st.title("Audio Recorder")
+    st.write("\n\n")
 
     # TUTORIAL: How to use STREAMLIT AUDIO RECORDER?
     # By calling this function, an instance of the audio recorder is created.
@@ -24,17 +25,20 @@ def audiorec_demo_app():
     # Add some spacing and informative messages
     col_info, col_space = st.columns([0.57, 0.43])
     with col_info:
-        st.write('\n' * 2)  # Add vertical spacer
-        st.write('The .wav audio data, as received in the backend Python code,'
-                 ' will be displayed below this message as soon as it has'
-                 ' been processed. [This informative message is not part of'
-                 ' the audio recorder and can be removed easily] 🎈')
+        st.write("\n" * 2)  # Add vertical spacer
+        st.write(
+            "The .wav audio data, as received in the backend Python code,"
+            " will be displayed below this message as soon as it has"
+            " been processed. [This informative message is not part of"
+            " the audio recorder and can be removed easily] 🎈"
+        )
 
     if wav_audio_data is not None:
         # Display audio data as received on the Python side
         col_playback, col_space = st.columns([0.58, 0.42])
         with col_playback:
-            st.audio(wav_audio_data, format='audio/wav')
+            st.audio(wav_audio_data, format="audio/wav")
+
 
 def chat_page():
     """
@@ -43,6 +47,7 @@ def chat_page():
     st.title("Chat Page")
     st.write("This is the Chat Page. You can chat with content here.")
     # Add a chat input box, and you can process and display chat messages here.
+
 
 # Conditionally render pages based on the selected page
 if page == "Audio Recorder":
