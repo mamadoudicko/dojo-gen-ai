d134b6e7f544785bbe5455f28587a4f4468d902e
mamadoudicko <mamadoudicko100@gmail.com>
2023-11-22 13:21:58 +0100

    feat: add recorder component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,57 @@
-import React from "react"
+import AudioReactRecorder, { RecordState } from 'audio-react-recorder';
+import 'audio-react-recorder/dist/index.css';
+import React, { useState } from "react";
+import { Streamlit } from "streamlit-component-lib";
 
 export const App = () => {
-  return <div>Ready to build your AI note taker ? Let's go 🚀🚀🔥</div>
-}
+  const [recordState, setRecordState] = useState(null);
+  const [audioDataURL, setAudioDataURL] = useState('');
+
+  const onClick = () => {
+    if (recordState === RecordState.START) {
+      setRecordState(RecordState.STOP);
+      return 
+    } 
+    setAudioDataURL('');
+    setRecordState(RecordState.START);
+  };
+
+  const onStop = (data) => {
+    setAudioDataURL(data.url);
+    data.blob.arrayBuffer().then((arrayBuffer) => {
+       const uint8Array = new Uint8Array(arrayBuffer);
+        Streamlit.setComponentValue({ "arr": uint8Array });
+     });
+  };
+  
+  const recordButtonLabel = recordState === RecordState.START ? 'Stop Recording' : 'Start Recording'
+
+  return (
+    <div>
+      <button 
+        id='toggle'
+        onClick={onClick} 
+        style={{
+          marginBottom: 10
+        }}
+      >
+        {recordButtonLabel}
+      </button>
+      
+      <AudioReactRecorder
+        state={recordState}
+        onStop={onStop}
+        canvasHeight={20}
+      />
+
+      <audio
+        id='audio'
+        controls
+        src={audioDataURL}
+        style={{
+          marginTop: 10
+        }}
+      />
+    </div>
+  );
+};
